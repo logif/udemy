a88b4a99f48ddbf50ffbb954658e059eaf394abd
logif <samyyklass@gmail.com>
2020-12-23 16:49:22 +0600

    Первый комментарий в проекте udemy

---
+++ Lessons/Udemy/js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// const result = confirm('You gay?');
+
+// const answer =  prompt('Ты кто?', 'Ваше имя');
+
+// console.log(typeof answer);
+
+const arr = [];
+
+arr[0] = prompt('Как тебя зовут?', '');
+arr[1] = prompt('Какая твоя фамилия?', '');
+arr[2] = prompt('Сколько тебе лет?', '');
+
+document.write(arr);
