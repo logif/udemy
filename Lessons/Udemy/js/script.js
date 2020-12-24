0db4a4e1e39448b92f1a467b149880690aafe677
Boris <samyyklass@gmail.com>
2020-12-24 12:40:02 +0600

    Переписал запросы в циклы

--- Lessons/Udemy/js/script.js
+++ Lessons/Udemy/js/script.js
@@ -10,12 +10,24 @@ const personalMovieDB = {
   privat: false,
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+  const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
+  
+  if ( a != null && b != null && a != '' && b != ''  && a.length < 50 ) {
+      personalMovieDB.movies[a] = b;
+    } else {
+      i--;
+    }
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+}
+
+if (personalMovieDB.count < 10) {
+  alert( 'Просмотрено довольно мало фильмов' );
+} else if (personalMovieDB.count > 10 || personalMovieDB.count < 30) {
+  alert( 'Вы классический зритель' );
+} else {
+  alert( 'Вы киноман' );
+}
 
 console.log(personalMovieDB); 
